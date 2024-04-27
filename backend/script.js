bd4428245e609f103b4090f4acae04fbf00317cf
GoncaloAzev14 <1211560@isep.ipp.pt>
2024-04-27 01:58:17 +0100

    openai api connection created

---
+++ backend/script.js
@@ -0,0 +1,22 @@
+import { config } from "dotenv"
+config()
+
+import { OpenAI } from "openai"
+import readline from "readline"
+
+const openAi = new OpenAI({ apiKey: process.env.API_KEY })
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+userInterface.prompt()
+userInterface.on("line", async input => {
+  const response = await openAi.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  })
+  console.log(response.data.choices[0].message.content)
+  userInterface.prompt()
+})
